b34c07ca4c8699697740092902506264f5875088
WILIAM WEBER <wiliam.weber@unidavi.edu.br>
2025-09-03 21:05:01 -0300

    Finalização da atividade!

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Registro do Service Worker para PWA
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
